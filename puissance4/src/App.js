f80876f99603f267c890016c70571a32d4c5e70e
Kevin GANEM <Ganemkevin@gmail.com>
2022-02-07 15:14:52 +0100

    check win functions

--- puissance4/src/App.js
+++ puissance4/src/App.js
@@ -4,6 +4,7 @@ import React from "react";
 import "./App.css";
 // COMPONENTS
 import Board from "./components/Board";
+import ResetButton from "./components/ResetButton";
 // BOOTSTRAPS
 import "bootstrap/dist/css/bootstrap.min.css";
 
--- puissance4/src/components/Board.js
+++ puissance4/src/components/Board.js
@@ -1,60 +1,189 @@
-// REACT
 import React from "react";
 
 class Board extends React.Component {
+  // States
   constructor() {
     super();
-
     this.state = {
       rows: 6,
       columns: 7,
+      moves: [],
+      playerTurn: "red",
+      // winner: "",
+      // moves: [
+      //   {
+      //     x: 0,
+      //     y: 0,
+      //     player: "red",
+      //   },
+      // ],
     };
   }
+  // Reset function
+  resetBoard = () => {
+    this.setState({
+      moves: [],
+    });
+  };
 
-  // INITIALIZE NEW GRID 6/7
-  renderGrid() {
-    let newGrid = [];
-    for (let x = 0; x < this.state.rows; x++) {
-      let newColumn = [];
-      for (let y = 0; y < this.state.columns; y++) {
-        newColumn.push(
+  getToken = (x, y) => {
+    const list = this.state.moves.filter((item) => {
+      return item.x === x && item.y === y;
+    });
+    return list[0];
+  };
+
+  checkForWin = (x, y, player) => {
+    // Check the horizontals
+    let winningMoves = [{ x, y }];
+    for (let column = x + 1; column < x + 4; column += 1) {
+      const checkToken = this.getToken(column, y);
+      if (checkToken && checkToken.player === player) {
+        winningMoves.push({ x: column, y: y });
+      } else {
+        break;
+      }
+    }
+
+    for (let column = x - 1; column > x - 4; column -= 1) {
+      const checkToken = this.getToken(column, y);
+      if (checkToken && checkToken.player === player) {
+        winningMoves.push({ x: column, y: y });
+      } else {
+        break;
+      }
+    }
+
+    if (winningMoves.length > 3) {
+      this.setState({ winner: player, winningMoves });
+      return true;
+    }
+
+    // Check the verticals
+    winningMoves = [{ x, y, player }];
+    for (let row = y + 1; row < y + 4; row += 1) {
+      const checkToken = this.getToken(x, row);
+      if (checkToken && checkToken.player === player) {
+        winningMoves.push({ x: x, y: row });
+      } else {
+        break;
+      }
+    }
+
+    for (let row = y - 1; row > y - 4; row -= 1) {
+      const checkToken = this.getToken(x, row);
+      if (checkToken && checkToken.player === player) {
+        winningMoves.push({ x: x, y: row });
+      } else {
+        break;
+      }
+    }
+
+    if (winningMoves.length > 3) {
+      this.setState({ winner: player, winningMoves });
+      return true;
+    }
+  };
+  addMove = (x, y) => {
+    const nextPlayerTurn = this.state.playerTurn === "red" ? "yellow" : "red";
+    // Below, we added a new move into our state, wherever we click on our board, and it will change the player's turn into the next player
+    //Check on a win based on this next move : we need to look at that position and all the adjacent positions in each direction, and determine if they are 4 in a row
+    // heckForWin () checks the position that we last added
+    this.setState(
+      {
+        moves: this.state.moves.concat({ x, y, player: this.state.playerTurn }),
+        playerTurn: nextPlayerTurn,
+      },
+      () => this.checkForWin({ x, y, player: this.state.playerTurn })
+    );
+  };
+
+  // To create dynamically our grid which is an array of arrays
+  renderBoard() {
+    const { winner } = this.state;
+    const rowViews = [];
+    for (let row = 0; row < this.state.rows; row += 1) {
+      const columnViews = [];
+      for (let column = 0; column < this.state.columns; column += 1) {
+        const token = this.getToken(column, row);
+        columnViews.push(
           <div
-            className="p-2"
+            onClick={() => {
+              this.addMove(column, row);
+            }}
             style={{
               width: "8vw",
               height: "8vw",
               backgroundColor: "blue",
-              border: "1px solid black",
               display: "flex",
+              padding: "5px",
+              cursor: "pointer",
             }}
           >
             <div
               style={{
                 borderRadius: "50%",
-                border: "1px solid black",
                 backgroundColor: "white",
                 flex: 1,
+                display: "flex",
               }}
-            ></div>
+            >
+              {token ? (
+                <div
+                  style={{
+                    backgroundColor: token.player,
+                    flex: 1,
+                    borderRadius: "50%",
+                  }}
+                ></div>
+              ) : undefined}
+            </div>
           </div>
         );
       }
-      newGrid.push(
-        <div className="" style={{ display: "flex" }}>
-          {newColumn}
-        </div>
-      );
+      rowViews.push(<div style={{ display: "flex" }}>{columnViews}</div>);
     }
-    return newGrid;
+    return (
+      <div
+        style={{
+          backgroundColor: "red",
+          display: "flex",
+          flexDirection: "column",
+        }}
+      >
+        {winner && (
+          <div
+            onClick={this.resetBoard}
+            style={{
+              position: "absolute",
+              backgroundColor: "rgba(0, 100, 0, .2)",
+            }}
+          >
+            {`${winner} WINS !!!`}
+          </div>
+        )}
+        {rowViews}
+      </div>
+    );
   }
 
   render() {
     return (
-      <div className="d-flex justify-content-center">
-        <div className="p-3">{this.renderGrid()}</div>
+      <div style={styles.container}>
+        <div>{this.renderBoard()}</div>
       </div>
     );
   }
 }
+
+const styles = {
+  container: {
+    height: "100%",
+    padding: 5,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+};
 
 export default Board;
