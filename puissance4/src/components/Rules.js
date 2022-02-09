2271f8728c607669b1777c849a3d0b9e1702494e
jess <jessica.elessa@gmail.com>
2022-02-09 05:26:01 +0100

    changed logo, border and separator colors,and fixed margins

--- puissance4/src/components/Rules.js
+++ puissance4/src/components/Rules.js
@@ -1,30 +1,44 @@
 // REACT
 import React from "react";
 // IMAGES
-import logo from "../assets/logo.png";
+// import logo from "../assets/logo.png";
 import children from "../assets/children.png";
 
 class Rules extends React.Component {
   render() {
     return (
       <aside className="gameRules">
-        <img className="logo" src={logo} alt="logo jeu"></img>
-        <img className="children" src={children} alt="children" />
-        <p className="ref">6 ans-Adulte</p>
-        <p className="ref players">2 joueurs</p>
+        {/* <img className="logo" src={logo} alt="logo jeu"></img> */}
+        <div className="logoTitle">
+          <p>
+            <span className="letterM">M</span>
+            <span className="letterB">B</span>
+          </p>
+          <p className="subtitle">JEUX</p>
+        </div>
+
+        <div className="ref">
+          <img className="children" src={children} alt="children" />
+          <p>6 ans-Adulte</p>
+          <p className="players">2 joueurs</p>
+        </div>
+
         <div className="separator"></div>
-        <p className="rules r1">
-          Insérez un jeton dans la grille à tour de rôle. Le premier joueur qui
-          réussit à aligner quatre jetons (horizontalement, verticalement ou
-          diagonalement) gagne la partie.
-        </p>
-        <p className="rules">
-          Si toutes les cases de la grille sont remplies et qu'aucun des deux
-          joueurs n'a réalisé un tel alignement, la partie est déclarée nulle.
-        </p>
-        <p className="rules">
-          Appuyez sur le bouton "Reset" pour commencer une nouvelle partie.
-        </p>
+
+        <div className="rules">
+          <p className="r1">
+            Insérez un jeton dans la grille à tour de rôle. Le premier joueur
+            qui réussit à aligner quatre jetons (horizontalement, verticalement
+            ou diagonalement) gagne la partie.
+          </p>
+          <p>
+            Si toutes les cases de la grille sont remplies et qu'aucun des deux
+            joueurs n'a réalisé un tel alignement, la partie est déclarée nulle.
+          </p>
+          <p className="rules">
+            Appuyez sur le bouton "Reset" pour commencer une nouvelle partie.
+          </p>
+        </div>
       </aside>
     );
   }
